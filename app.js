ab9abf0da4eed254d6062aabeceafde067f33a00
g00091881 <g00091881@aus.edu>
2025-03-01 16:04:59 +0400

    third commit

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const express = require('express')
 const app = express()
+var cors = require('cors')
 const port = 8080
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
+var corsOptions = {
+    origin: '*',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+
+app.get('/', cors(corsOptions), function (req, res, next) {
+    res.send({msg: 'This is CORS-enabled for only example.com.'})
 })
 
 app.listen(port, () => {
